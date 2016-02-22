571a8371bfe2848aac42bdc70e67cee503625e97
vtnate02 <vtnate02@gmail.com>
2016-02-21 20:41:16 -0500

    Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+##FUNCTIONSET PURPOSE
+##Compute the inverse of a matrix once, and cache it rather than rerun the computation each time
 
-## Write a short comment describing this function
-
+## Function 1 - makeCacheMatrix
+##Define list of functions to evaluate/retrieve matrix and its potentially cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## 1-Define evironment object for inverse
+  mat_inv <- NULL
+  ## 2.1-Define set matrix function
+  set <- function(y) {
+    x <<- y
+    mat_inv <<- NULL
+  }
+  ## 2.2-Define get matrix function
+  get <- function() x
+  ## 2.3-Define store inverse function
+  setinv <- function(inverse) mat_inv <<- inverse
+  ## 2.4-Define cached inverse retrieval function
+  getinv <- function() mat_inv
+  ## 3-Return the list of functions
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
-
-## Write a short comment describing this function
-
+##Function 2 - cashSolve
+##Looks to see if inverse has already been cached
+##if yes, pull from cache, if no compute and return
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##Get cached inverse value using getinv function
+  mat_inv <- x$getinv()
+  ##Is inverse null?
+  if(!is.null(mat_inv)) {
+    ##Y ->Call getinv function to retrieve cached inverse
+    message("Retrieved from Cache")
+    return(mat_inv)
+  }
+  ##N ->
+  ## 1-Call Get function to retreive matrix
+  temp <- x$get()
+  ## 2- Use solve() to take matrix and return inverse
+  mat_inv <- solve(temp) 
+  ## 3- Cache inverse
+  x$setinv(mat_inv)
+  ## 4- Return inverse
+  mat_inv
 }
